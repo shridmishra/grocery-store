0315f4ede1487eccb3ff4112f08cdfbee3672647
shridmishra <shridmishra00@gmail.com>
2025-08-01 23:21:29 +0530

    feat: update implementation (authUser.js)

--- server/middleware/authUser.js
+++ server/middleware/authUser.js
@@ -4,20 +4,19 @@ const authUser = async (req, res, next) => {
   const token = req.cookies?.token;
 
   if (!token) {
-    return res.json({ success: false, msg: "No token. Not authorized" });
+    return res.status(401).json({ success: false, msg: "No token. Not authorized" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded.id) {
       req.userId = decoded.id;
+      next(); 
     } else {
-      res.json({ success: false, msg: "Not Authorized " });
+      return res.status(401).json({ success: false, msg: "Not Authorized" });
     }
-
-    next();
   } catch (error) {
-    return res.json({ success: false, error: "Invalid or expired token" });
+    return res.status(401).json({ success: false, error: "Invalid or expired token" });
   }
 };
 
